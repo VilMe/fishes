50a577a3687cce5945c57c9a3a19a759f0021ac0
Vilma M <vilma.medrano00@gmail.com>
2019-08-31 15:43:51 -0700

    define shark and clownsih classes, using inheritance and super() method

--- fish.py
+++ fish.py
@@ -15,15 +15,55 @@ class Fish:
         print("The fish can swim backwards.")
 
 class Trout(Fish):
-    pass
+    def __init__(self, water="freshwater"):
+        self.water = water
+        super().__init__(self)
+
+class Clownfish(Fish):
+    """docstring for Clownfish"""
+    
+    def live_with_anenome(self):
+        print("The clownfish is coexisting with sea anenome.")
+        
 
 
+class Shark(Fish):
+    """docstring for Shark"""
+    def __init__(self, first_name, last_name="Shark",
+                 skeleton="cartilage", eyelids=True):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.skeleton = skeleton
+        self.eyelids = eyelids
 
-terry = Trout("Terry")
+    def swim_backwards(self):
+        print("The shark cannot swim backwards, but can sink backwards.")
+
+
+        
+
+# terry = Trout("Terry")
+# print(terry.first_name + " " + terry.last_name)
+
+
+# casey = Clownfish("Casey")
+# print(casey.first_name + " " + casey.last_name)
+# casey.swim()
+# casey.live_with_anenome()
+
+
+# sammy = Shark("Sammy")
+# print(sammy.first_name + " " + sammy.last_name)
+# sammy.swim()
+# sammy.swim_backwards()
+# print(sammy.eyelids)
+# print(sammy.skeleton)
+
+terry = Trout()
+terry.first_name = "Terry"
+
 print(terry.first_name + " " + terry.last_name)
-print(terry.skeleton)
 print(terry.eyelids)
-terry.swim()
-terry.swim_backwards
+print(terry.water)
 
 
